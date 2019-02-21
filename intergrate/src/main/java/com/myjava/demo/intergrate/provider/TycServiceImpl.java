6cb82b3d04f0558b14bd92cb4eb4b7ed5b824f96
HWliao <499606788@qq.com>
2019-02-21 14:29:08 +0800

    chore: 需要自己集成dubbox auto configuration了

--- intergrate/src/main/java/com/myjava/demo/intergrate/provider/TycServiceImpl.java
+++ intergrate/src/main/java/com/myjava/demo/intergrate/provider/TycServiceImpl.java
@@ -1,9 +1,13 @@
 package com.myjava.demo.intergrate.provider;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.jjshome.kp.api.service.institution.IInstitutionService;
 import com.myjava.demo.intergrate.tyc.service.ITycService;
-import org.apache.dubbo.config.annotation.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
 
 /**
  * 天眼查服务实现
@@ -14,6 +18,15 @@ import org.slf4j.LoggerFactory;
 @Service(version = "1.0")
 public class TycServiceImpl implements ITycService {
   private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @Reference(timeout = 50000)
+  private IInstitutionService iInstitutionService;
+
+  @PostConstruct
+  public void init() {
+    Integer integer = this.iInstitutionService.qryAgencyCount();
+    System.out.println(integer);
+  }
 
   public void sayHello() {
     System.out.println("hello");
