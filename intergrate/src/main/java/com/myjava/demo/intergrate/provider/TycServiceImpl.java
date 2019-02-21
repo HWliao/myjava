98b19b81872661c26ece51627e29c9c88c1e9768
HWliao <499606788@qq.com>
2019-02-21 15:11:23 +0800

    chore: 修改命名

--- intergrate/src/main/java/com/myjava/demo/intergrate/provider/TycServiceImpl.java
+++ intergrate/src/main/java/com/myjava/demo/intergrate/provider/TycServiceImpl.java
@@ -1,13 +1,9 @@
 package com.myjava.demo.intergrate.provider;
 
-import com.alibaba.dubbo.config.annotation.Reference;
-import com.alibaba.dubbo.config.annotation.Service;
-import com.jjshome.kp.api.service.institution.IInstitutionService;
 import com.myjava.demo.intergrate.tyc.service.ITycService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
 
 /**
  * 天眼查服务实现
@@ -15,18 +11,9 @@ import javax.annotation.PostConstruct;
  * @author lhw
  * @date 2019/2/21
  */
-@Service(version = "1.0")
+@Service
 public class TycServiceImpl implements ITycService {
   private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-  @Reference(timeout = 50000)
-  private IInstitutionService iInstitutionService;
-
-  @PostConstruct
-  public void init() {
-    Integer integer = this.iInstitutionService.qryAgencyCount();
-    System.out.println(integer);
-  }
 
   public void sayHello() {
     System.out.println("hello");
--- intergrate/src/test/java/com/myjava/demo/intergrate/DemoCloudClientApplicationTests.java
+++ intergrate/src/test/java/com/myjava/demo/intergrate/DemoCloudClientApplicationTests.java
@@ -1,10 +1,5 @@
 package com.myjava.demo.intergrate;
 
-import com.alibaba.dubbo.config.annotation.Reference;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jjshome.kp.api.service.institution.IInstitutionService;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,12 +13,4 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoCloudClientApplicationTests {
-  @Reference
-  private IInstitutionService iInstitutionService;
-
-  @Test
-  public void Test1() throws JsonProcessingException {
-    ObjectMapper objectMapper = new ObjectMapper();
-    System.out.println(objectMapper.writeValueAsString(this.iInstitutionService.qryAgencyCount()));
-  }
 }
