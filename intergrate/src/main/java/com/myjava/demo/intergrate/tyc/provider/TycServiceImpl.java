42ef3449cefbde007c343731cc133a90712a8ffe
HWliao <499606788@qq.com>
2019-02-25 15:11:34 +0800

    feat: 加入dubbox-spring-boot-starter

--- intergrate/src/main/java/com/myjava/demo/intergrate/tyc/provider/TycServiceImpl.java
+++ intergrate/src/main/java/com/myjava/demo/intergrate/tyc/provider/TycServiceImpl.java
@@ -1,10 +1,13 @@
 package com.myjava.demo.intergrate.tyc.provider;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.config.annotation.Service;
 import com.jjshome.kp.api.service.institution.IInstitutionService;
 import com.myjava.demo.intergrate.tyc.service.ITycService;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
@@ -14,8 +17,11 @@ import javax.annotation.PostConstruct;
  * @author lhw
  * @date 2019/2/21
  */
+@Service
+@Component
 public class TycServiceImpl implements ITycService {
   private Logger logger = LoggerFactory.getLogger(this.getClass());
+  @Reference
   private IInstitutionService iInstitutionService;
 
   @PostConstruct
